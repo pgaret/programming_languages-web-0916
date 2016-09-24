17d05aa51bf856d43b1172f4cad69b16f0a8f717
Peregrin <peregringaret@gmail.com>
2016-09-24 03:35:56 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  nLang = {}
+  languages.each do |a, b|
+    b.each do |c, d|
+      if !nLang.keys.include?(c)
+        nLang[c] = d
+        nLang[c][:style] = Array(a)
+      else
+        nLang[c][:style].push(a)
+      end
+    end
+  end
+  nLang
 end
